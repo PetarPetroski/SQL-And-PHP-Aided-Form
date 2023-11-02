758562a1efa5d2572c15a42f4c55d887fc0a8798
Petar Petroski <petar.petroski2003@gmail.com>
2023-11-01 23:26:25 -0400

    Added PHP files

---
+++ process.php
@@ -0,0 +1,15 @@
+<?php
+require 'connect.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>House of Health</title>
+</head>
+<body>
+    <!-- Your HTML code for buttons and other content goes here -->
+</body>
+</html>
