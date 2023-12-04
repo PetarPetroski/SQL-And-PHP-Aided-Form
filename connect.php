805aba2dfb43855fa955775f929125fd4ff65185
Petar Petroski <45236464+PetarPetroski@users.noreply.github.com>
2023-12-04 15:10:16 -0500

    Delete connect.php

--- connect.php
+++
@@ -1,8 +0,0 @@
-<?php
-//Makes DB connection
-$servername = "sql1.njit.edu";
-$username = "pp79";
-$password = "Bubaaron10$";
-$dbname = "pp79";
-$con = mysqli_connect($servername,$username,$password,$dbname);
-?>
